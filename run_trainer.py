78755f026a8db86b9cb40d87c0d8cef34eb09f72
Salman Malik <salmalik64@gmail.com>
2023-05-26 12:07:08 +0800

    added mps support - traniner_mac.py

--- run_trainer.py
+++ run_trainer.py
@@ -3,22 +3,36 @@ import sys
 
 sys.dont_write_bytecode = True
 
-import torch
-import os
 from core.config import Config
 from core import Trainer
 
-
-def main(rank, config):
-    trainer = Trainer(rank, config)
+if __name__ == "__main__":
+    config = Config("/Users/salmanmalik/Documents/MPhil/LibFewShot/config/test_install.yaml").get_config_dict()
+    rank = 0  # Set the rank to 0 for single GPU or CPU
+    trainer = Trainer(rank, config)  # Pass both rank and config arguments
     trainer.train_loop(rank)
 
+# # -*- coding: utf-8 -*-
+# import sys
 
-if __name__ == "__main__":
-    config = Config("./config/proto.yaml").get_config_dict()
+# sys.dont_write_bytecode = True
 
-    if config["n_gpu"] > 1:
-        os.environ["CUDA_VISIBLE_DEVICES"] = config["device_ids"]
-        torch.multiprocessing.spawn(main, nprocs=config["n_gpu"], args=(config,))
-    else:
-        main(0, config)
+# import torch
+# import os
+# from core.config import Config
+# from core import Trainer
+
+
+# def main(rank, config):
+#     trainer = Trainer(rank, config)
+#     trainer.train_loop(rank)
+
+
+# if __name__ == "__main__":
+#     config = Config("./config/proto.yaml").get_config_dict()
+
+#     if config["n_gpu"] > 1:
+#         os.environ["CUDA_VISIBLE_DEVICES"] = config["device_ids"]
+#         torch.multiprocessing.spawn(main, nprocs=config["n_gpu"], args=(config,))
+#     else:
+#         main(0, config)
