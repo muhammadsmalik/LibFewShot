ca5036e8479fd3a0a5510dd265548520f3d7b649
Chew Boon Zhan <chewboonzhan@gmail.com>
2023-06-03 23:36:00 +0800

    Updated test run

--- run_test.py
+++ run_test.py
@@ -12,7 +12,7 @@ from core import Test
 PATH = "./results/ProtoNet-consolidated_seeds_dataset-Conv64F-2-1-Jun-03-2023-15-20-14"
 VAR_DICT = {
     "test_epoch": 5,
-    "n_gpu": 0,
+    "n_gpu": 1,
     "test_episode": 600,
     "episode_size": 1,
 }
