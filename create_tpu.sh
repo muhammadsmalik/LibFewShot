6a16d1cc92d1e7a133ca8b611b0ee69ecbd0614e
Salman Malik <73635672+muhammadsmalik@users.noreply.github.com>
2023-06-10 21:53:13 +0800

    updated create_tpu script

--- create_tpu.sh
+++ create_tpu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-max_attempts=500
+max_attempts=5
 sleep_time=10 # sleep for 60 seconds between attempts
 
 # Loop through instance names ocd3-2, ocd3-3, ocd3-4, ocd3-5
-for instance in ocd3-2 ocd3-3 ocd3-4 ocd3-5; do
+for instance in od3-2 od3-5; do
   attempt=0
 
   while true; do
     attempt=$((attempt + 1))
 
     # Run the gcloud command
-    gcloud compute tpus tpu-vm create "$instance" --zone=europe-west4-a --accelerator-type=v3-8 --version=tpu-vm-pt-2.0
+    gcloud compute tpus tpu-vm create "$instance" --zone=us-central1-a --accelerator-type=v3-8 --version=tpu-vm-pt-2.0
 
     # Check if the command was successful
     if [ $? -eq 0 ]; then
